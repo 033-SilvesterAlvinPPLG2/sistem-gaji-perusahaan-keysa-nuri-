398781d045b7e13d0636af4d872128dc966c8dee
Akv1gn <oktavianoalvin1@gmail.com>
2025-02-19 20:36:13 +0700

    sistem gajian perusahaan

---
+++ studigaji.js
@@ -0,0 +1,18 @@
+//penggajian
+    let gajiPokok = 5000000
+    let bonus = 500000
+    let hadir = 200000
+    let i = 0;
+
+for (let i = 0; i < 20; i++) {
+    var totalGaji= gajiPokok+hadir;
+    console.log(totalGaji);
+
+}
+
+console.log(`tambahan jika kamu masuk full 20hari kedepan ${i+bonus}`)
+
+let gajiKeseluruhan = totalGaji+bonus;
+console.log(`total keseluruhan`)
+
+console.log(`gaji keseluruhan + bonus anda adalah Rp` + gajiKeseluruhan);
